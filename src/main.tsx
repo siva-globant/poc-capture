e88890ab9483a906565fe81efb6298560ecbeae1
siva.s <siva.s@globant.com>
2023-08-21 15:39:46 +0530

    update(context): sentry context used

--- src/main.tsx
+++ src/main.tsx
@@ -2,38 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import { MonitoringProvider } from "./context";
-import * as Sentry from "@sentry/react";
+import { MonitoringProvider } from "./context";
+// import * as Sentry from "@sentry/react";
 
-// const sentryConfig = {
-//   dsn: "https://6452d0c6c4bda6947b3186da5506fba3@o4505669501648896.ingest.sentry.io/4505679649177600",
-//   environment: import.meta.env.MODE,
-//   debug: import.meta.env.PROD,
-//   tracesSampleRate: 0.025,
-//   release: `poc-video-capture-native`,
-//   tracingOrigins: ["localhost", "https://siva-globant.github.io"],
-// };
-Sentry.init({
+const sentryConfig = {
   dsn: "https://6452d0c6c4bda6947b3186da5506fba3@o4505669501648896.ingest.sentry.io/4505679649177600",
-  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
+  environment: import.meta.env.MODE,
+  debug: import.meta.env.PROD,
   tracesSampleRate: 1.0,
+  release: `poc-video-capture-native`,
+  tracingOrigins: ["localhost", "https://siva-globant.github.io"],
+};
+// Sentry.init({
+//   dsn: "https://6452d0c6c4bda6947b3186da5506fba3@o4505669501648896.ingest.sentry.io/4505679649177600",
+//   integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
 
-  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost"],
+//   // Set tracesSampleRate to 1.0 to capture 100%
+//   // of transactions for performance monitoring.
+//   tracesSampleRate: 1.0,
 
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+//   // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+//   tracePropagationTargets: ["localhost"],
+
+//   // Capture Replay for 10% of all sessions,
+//   // plus for 100% of sessions with an error
+//   replaysSessionSampleRate: 0.1,
+//   replaysOnErrorSampleRate: 1.0,
+// });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <MonitoringProvider config={sentryConfig}> */}
-    <App />
-    {/* </MonitoringProvider> */}
+    <MonitoringProvider config={sentryConfig}>
+      <App />
+    </MonitoringProvider>
   </React.StrictMode>
 );
